2297b4323ec8f271a47ce4fdde53d5b5b5ebcd6b
Aseem Savio <aseemsavio3@gmail.com>
2020-03-10 02:26:59 +0530

    AVL insert operation half way

--- src/main/java/avltree/App.java
+++ src/main/java/avltree/App.java
@@ -4,5 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         AvlTree tree = new AvlTree();
+        System.out.println(tree.isNull() ? "Tree is empty" : "Tree is NOT empty");
+
     }
 }
--- src/main/java/avltree/AvlTree.java
+++ src/main/java/avltree/AvlTree.java
@@ -11,6 +11,34 @@ public class AvlTree {
         this.root = null;
     }
 
+    /**
+     * Checks if the tree is null or not
+     *
+     * @return
+     */
+    public boolean isNull() {
+        return (this.root == null);
+    }
+
+    /**
+     * Inserts a new node with the given data
+     *
+     * @param dataToInsert
+     */
+    public void insert(int dataToInsert) {
+        root = insert(root, dataToInsert);
+    }
+
+    private Node insert(Node node, int dataToInsert) {
+        if (node == null)
+            return new Node(dataToInsert);
+        if (dataToInsert < node.getData())
+            node.setLeft(insert(node.getLeft(), dataToInsert));
+        else if (dataToInsert > node.getData())
+            node.setRight(insert(node.getRight(), dataToInsert));
+        return node;
+    }
+
     public Node getRoot() {
         return root;
     }
