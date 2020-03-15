3bc86ea7ea66b037ecf67c5fdbff405de9510d02
Aseem Savio <aseemsavio3@gmail.com>
2020-03-15 22:33:14 +0530

    Binary Heap

--- src/main/java/binaryheap/App.java
+++ src/main/java/binaryheap/App.java
@@ -4,9 +4,6 @@ public class App {
 
     public static void main(String[] args) {
 
-        Heap heap = new BinaryMinHeap(10);
-        heap.insert(1);
-
     }
 
 }
--- src/main/java/binaryheap/BinaryHeap.java
+++ src/main/java/binaryheap/BinaryHeap.java
@@ -1,90 +1,18 @@
 package binaryheap;
 
-public abstract class BinaryHeap implements Heap {
+public class BinaryHeap {
 
-    protected int[] heap;
-    protected int heapSize;
-    protected int maxSizeOfHeap;
-    protected final int first = 1;
+    private final int FRONT = 1;
+    private int heapSize;
+    private int maxSize;
+    private final int TWO = 2;
+    private int[] heap;
 
-    public BinaryHeap(int maxSizeOfHeap) {
-        this.maxSizeOfHeap = maxSizeOfHeap;
-        heap = new int[maxSizeOfHeap + 1];
+    public BinaryHeap(int maxSize) {
+        this.maxSize = maxSize;
+        heap = new int[maxSize];
+        heap[0] = Integer.MIN_VALUE;
     }
 
-    @Override
-    public void insert(int dataToInsert) {}
 
-    @Override
-    public void remove(int dataToRemove) {}
-
-    @Override
-    public void peek(int dataToPeek) {}
-
-    @Override
-    public void extract(int dataToExtract) {}
-
-    /**
-     * parent = position / 2
-     *
-     * @param position
-     * @return
-     */
-    protected int getParent(int position) {
-        return heap[position / 2];
-    }
-
-    protected int getparentPosition(int position) {
-        return position / 2;
-    }
-
-    /**
-     * Left Child = position * 2
-     *
-     * @param position
-     * @return
-     */
-    protected int getLeftChild(int position) {
-        return heap[position * 2];
-    }
-
-    protected int getLeftChildPosition(int position) {
-        return position * 2;
-    }
-
-    /**
-     * Right Child = position * 2 + 1
-     *
-     * @param position
-     * @return
-     */
-    protected int getRightChild(int position) {
-        return heap[position * 2 + 1];
-    }
-
-    protected int getRightChildPosition(int position) {
-        return position * 2 + 1;
-    }
-
-    /**
-     * Finds if a given node is a leaf or not
-     *
-     * @param position
-     * @return
-     */
-    protected boolean isLeaf(int position) {
-        return (position >= (heapSize / 2) && position <= heapSize);
-    }
-
-    /**
-     * Swaps two nodes
-     *
-     * @param initialPosition
-     * @param newPosition
-     */
-    protected void swap(int initialPosition, int newPosition) {
-        int temp = heap[initialPosition];
-        heap[initialPosition] = heap[newPosition];
-        heap[newPosition] = temp;
-    }
 }
