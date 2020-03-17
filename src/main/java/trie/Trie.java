a0f8eaadc7b92a5db4a1d7af6029a4cb5dbf1946
Aseem Savio <aseemsavio3@gmail.com>
2020-03-17 10:11:59 +0530

    trie

--- src/main/java/trie/Trie.java
+++ src/main/java/trie/Trie.java
@@ -1,4 +1,17 @@
 package trie;
 
 public class Trie {
+
+    public void insert(String word) {
+
+    }
+
+    public void delete(String word) {
+
+    }
+
+    public void query(String word) {
+
+    }
+
 }
